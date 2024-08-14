e61434359948eb1481445a0427b60222d70cd5e7
manna84 <mannasingh84@gmail.com>
2024-08-14 06:07:06 +0530

    Fix:Mobile view crousel

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import hugevid from "../../assets/huge-vid.mp4";
-// import NavBar from "../NavBar/NavBar";
-// import { restaurantimg1 } from "../../assets/restaurant-1.jpg";
-// import { restaurantimg2 } from "../../assets/restaurant-2.jpg";
-// import { restaurantimg3 } from "../../assets/restaurant-3.jpg";
+import NavBar from "../NavBar/NavBar";
+import restaurantimg1 from "../../assets/restaurant-1.jpg";
+import restaurantimg2 from "../../assets/restaurant-2.jpg";
+import restaurantimg3 from "../../assets/restaurant-3.jpg";
+import { Fade } from "react-slideshow-image";
 
 function Header() {
+  const heroImages = [restaurantimg1, restaurantimg2, restaurantimg3];
+  const [imageIndex, setImageIndex] = useState(0);
+
+  useEffect(() => {
+    const slideInterval = setInterval(() => {
+      setImageIndex((prev) => (prev === heroImages.length - 1 ? 0 : prev + 1));
+    }, 3000);
+    return () => clearInterval(slideInterval);
+  }, [heroImages.length]);
+  console.log(imageIndex);
+
   return (
-    <div className="relative h-screen overflow-hidden">
+    <div className="relative h-screen overflow-hidden ]">
       {/* <NavBar /> */}
       <video
         autoPlay
         loop
         muted
-        className="fixed top-0 left-0 w-full h-full object-cover z-0"
+        className="fixed top-0 left-0 w-full h-full object-cover z-0 hidden md:block"
       >
         <source src={hugevid} />
       </video>
+      <Fade
+        arrows={false}
+        duration={1000}
+        transitionDuration={500}
+        tras
+        easing={"ease"}
+      >
+        <img
+          src={heroImages[imageIndex]}
+          alt=""
+          className="w-full h-full object-cover fixed"
+        />
+      </Fade>
     </div>
   );
 }
